f4d8bfc86b7a315935c734159e368a3650c64f50
JoiceStefany <steffanyjoice006@gmail.com>
2023-10-13 15:33:10 -0300

    Transaction Service

--- src/main/java/com/picpaysimplificado/Repository/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/Repository/TransactionRepository.java
@@ -1,7 +1,9 @@
-package com.picpaysimplificado.Domin.Transaction;
+package com.picpaysimplificado.Repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplificado.Domin.Transaction.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
